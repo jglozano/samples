fe939839557b04ba00a0246554cb8d0d8a2547a3
Javier Lozano <javier@lozanotek.com>
2010-04-16 00:04:47 -0500

    Removed the DataAnnotations for the input model.

--- JSONMvc/JSONMvcSample/Models/PersonInputModel.cs
+++ JSONMvc/JSONMvcSample/Models/PersonInputModel.cs
@@ -1,10 +1,8 @@
 ﻿namespace JSONMvcSample.Models {
     using System;
-    using System.ComponentModel.DataAnnotations;
 
     [Serializable]
     public class PersonInputModel {
-        [Required]
         public string Name { get; set; }
         public int Age { get; set; }
     }
